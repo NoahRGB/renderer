6902c1d225bd6b96a97c61ea65e349026056f8b1
NoahRGB <ncox2004@gmail.com>
2025-06-28 12:32:41 +0100

    basic shaders

--- Renderer.cpp
+++ Renderer.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 
+#include "Shader.h"
+
 void input(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
@@ -12,6 +14,17 @@ void input(GLFWwindow* window) {
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
+}
+
+void shaderCompilationSuccessCheck(unsigned int& shader) {
+	// checks if the given shader compiled successfully
+	int success;
+	char infoLog[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "Shader compilation failed\n" << infoLog << std::endl;
+	}
 }
 
 int main(void) {
@@ -41,13 +54,61 @@ int main(void) {
 	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	Shader shader("shaders/vertex.shader", "shaders/fragment.shader");
+
+	// setup
+	float vertices[] = {
+		0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+	};
+
+	unsigned int indices[] = {
+		0, 1, 2
+	};
+
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+	// bind the VAO to store the subsequent data
+	glBindVertexArray(VAO);
+
+	// copy vertex data to VBO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// bind the indices to the EBO (which will be stored in the currently bound VAO)
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// set vertex attributes from the currently bound VBO (which will be stored in the currently bound VAO)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+
 	// render loop
 	while (!glfwWindowShouldClose(window)) {
 		input(window);
 
 		// rendering
-		glClearColor(0.2f, 0.3, 0.3f, 1.0f);
+		glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		shader.use();
+
+		// load the VAO that contians the vertex data and the indices to draw
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		// check for events, swap buffers
 		glfwSwapBuffers(window);
